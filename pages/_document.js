53d5e2ddc8d6ab148057c12ba6196ca0262a99e3
Jack Hsin <qk14000@gmail.com>
2020-06-04 03:44:06 -0700

    Add google-site-verification Meta in to _document

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="google-site-verification" content="EePNiz9uww5d8uZsrGWGNNhNiwlwCemxIXkAozjQuyE" />
           <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet"></link>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
